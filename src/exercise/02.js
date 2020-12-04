be54c1fe2d8f0c8adcb0faf12e05abfce0840a3e
Ahmed Alani <s3leha@gmail.com>
2020-12-04 12:53:37 -0800

    exercise 2 done with all the extra credit, custom hook practice

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,23 +3,52 @@
 
 import * as React from 'react'
 
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
+  const [state, setState] = React.useState(()=>{
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      return deserialize(valueInLocalStorage)
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+  const prevKeyRef = React.useRef(key)
+  React.useEffect(()=>{
+    const prevKey = prevKeyRef.current
+    if(prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
+  return [state, setState]
+}  
+
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  // const [name, setName] = React.useState(()=> window.localStorage.getItem('name') || initialName)
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  // React.useEffect(()=>{
+  //   window.localStorage.setItem('name', name)
+  // }, [name]);
+
+  // this is extra; practicing custom hooks
+  const [name, setName] = useLocalStorageState('name', initialName);
 
   function handleChange(event) {
+    event.preventDefault()
     setName(event.target.value)
   }
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
